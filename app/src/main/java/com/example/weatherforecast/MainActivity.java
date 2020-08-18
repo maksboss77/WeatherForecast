121cd37e3ba78d13de9e2778c692a4f0c7e8bd62
Макс Савельев <maksboss77@gmail.com>
2020-08-18 17:44:05 +0700

    Query using AsyncTask, only 1 element works

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.weatherforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,14 +23,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Создаем список фейковых дней
-        ArrayList<Weather> weathers = QueryUtils.extractWeaters();
 
-        ListView weatherListView = (ListView) findViewById(R.id.list);
-
-        WeatherAdapter adapter = new WeatherAdapter(this, 0 ,weathers);
-
-        weatherListView.setAdapter(adapter);
+        WeatherAsyncTask task = new WeatherAsyncTask();
+        task.execute();
 
     }
+
+    private class WeatherAsyncTask extends AsyncTask<ListView, Void, WeatherAdapter> {
+
+        ListView weatherListView;
+
+        @Override
+        protected WeatherAdapter doInBackground(ListView... listViews) {
+            //Создаем список фейковых дней
+            ArrayList<Weather> weathers = QueryUtils.extractWeathers();
+
+            weatherListView = (ListView) findViewById(R.id.list);
+
+            WeatherAdapter adapter = new WeatherAdapter(MainActivity.this, 0, weathers);
+
+            return adapter;
+        }
+
+        @Override
+        protected void onPostExecute(WeatherAdapter adapter) {
+
+            weatherListView.setAdapter(adapter);
+        }
+    }
+
 }
--- app/src/main/java/com/example/weatherforecast/QueryUtils.java
+++ app/src/main/java/com/example/weatherforecast/QueryUtils.java
@@ -6,24 +6,35 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public final class QueryUtils {
 
-    private static final String SAMPLE_JSON_RESPONSE = "";
+
+    private static final String WEATHER_RESPONSE_URL =
+            "http://api.openweathermap.org/data/2.5/forecast?q=Novokuznetsk,ru&lang=ru&units=metric&appid=31b762ad9bd0b94b1c2a3cecee08e837";
 
     // Частный конструктор - то есть никто не должен создавать экземпляр этого класса.
     private QueryUtils() {
 
     }
 
-    public static ArrayList<Weather> extractWeaters() {
+    public static ArrayList<Weather> extractWeathers() {
 
         ArrayList<Weather> weathers = new ArrayList<>();
 
         try {
 
-            JSONObject baseJsonResponse = new JSONObject(SAMPLE_JSON_RESPONSE);
+            JSONObject baseJsonResponse = new JSONObject(getStringJSON(WEATHER_RESPONSE_URL));
             JSONArray weatherArrayList = baseJsonResponse.getJSONArray("list");
 
             for (int i = 0; i < weatherArrayList.length(); i++) {
@@ -49,7 +60,7 @@ public final class QueryUtils {
                 int clouds = cloudsObject.getInt("all");
 
                 JSONObject windObject = currentWeatherObject.getJSONObject("wind");
-                int wind = cloudsObject.getInt("speed");
+                int wind = windObject.getInt("speed");
 
                 Weather weather = new Weather(date, temp, pressure, clouds, wind, humidity, description, icon);
                 weathers.add(weather);
@@ -58,10 +69,66 @@ public final class QueryUtils {
 
         } catch (JSONException e) {
             Log.e("QueryUtils", "Problem parsing the weather JSON results", e);
+        } catch (Exception e) {
+            Log.e("QueryUtils", "Problem URL connect", e);
         }
 
 
         return weathers;
+    }
+
+    public static String getStringJSON(String urlString) throws IOException {
+
+        String jsonResponse = "";
+
+        if (urlString == null) {
+            return jsonResponse;
+        }
+
+        URL url = null;
+
+        HttpURLConnection urlConnection = null;
+        StringBuilder stringBuilder = null;
+        InputStream inputStream = null;
+
+        try {
+            url = new URL(urlString);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.connect(); //тут вылетает
+
+            stringBuilder = new StringBuilder();
+
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                BufferedReader reader= new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+            } else {
+                Log.e("QueryUtils", "Error response code: " + urlConnection.getResponseCode());
+            }
+
+
+        } catch (IOException e) {
+            Log.e("QueryUtils", "Problem URLConnection or inputStream connection", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                inputStream.close();
+            }
+        }
+
+        return stringBuilder.toString();
     }
 
 
